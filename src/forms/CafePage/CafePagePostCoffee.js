530a44ad0a952a3112c2a56f6220647914ca5433
rakeshsenapathi <rakeshsenapathi@gmail.com>
2021-05-22 11:33:39 +0530

    Conversation Page Complete

--- src/forms/CafePage/CafePagePostCoffee.js
+++ src/forms/CafePage/CafePagePostCoffee.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import "./CafePage.scss";
+import image from "../../assets/images/conversation-1.png";
 
 function CafePagePostCoffee() {
+    const playerName = "Rakesh"; //Mocking name, take it from context-api next
     return (
-        <div className="coffeeShopStory">
-            Load Conversation here
+        <div className="cafePagePostCoffee">
+            <div className="cafePagePostCoffee__conversation">
+                <img className="cafePagePostCoffee__image"
+                    alt="Coversation" src={image}
+                />
+                <p className="conversation-text conversation-text--1">
+                    {playerName}: <br />
+                    Here is the password<br />
+                    for the wifi router
+                </p>
+                <p className="conversation-text conversation-text--2">
+                    Hacker: Ah good job! See how easy <br />
+                    that was. I have now hacked <br />
+                    into Tom’s company’s laptop <br />
+                    and found some very <br />
+                    interesting personal messages <br />
+                    and pictures that may cause <br />
+                    Tom a lot of embarrassment. <br />
+                    Perfect material to blackmail <br />
+                    him into giving us <br />
+                    company credentials. <br />
+                </p>
+                <p className="conversation-text conversation-text--3">
+                    Task 1 complete, we now have <br />
+                    company Sigma’s employee <br />
+                    details to go into their <br />
+                    system. Next task, go to<br />
+                    company Sigma’s office. <br />
+                </p>
+                <button className="conversation-btn btn btn--default">Next</button>
+            </div>
         </div>
     );
 };
