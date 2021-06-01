7b1edfe4749b17b493028692cde39bbd3580bdea
rakeshsenapathi <rakeshsenapathi@gmail.com>
2021-06-01 23:19:22 +0530

    new tab open

--- src/forms/FireAlarm/EndCredits.js
+++ src/forms/FireAlarm/EndCredits.js
@@ -9,10 +9,10 @@ function EndCredits() {
             </div>
             <p className="end-credits__text--1">Hope you had fun whilst learning about the key issues of the latest cyber security threats. Now that you can think like a hacker, you can be more alert and vigilant so that you don’t.</p>
             <p className="end-credits__text--2">Click here for more resources to follow and stay up-to-date with your cyber security training:</p>
-            <a className="end-credits__text--anchor" href="https://www.notion.so/f3dec368d75b427e9d7d08636876cafa?v=c10e1d052a864d31b81ea1890e783f5a
+            <a className="end-credits__text--anchor" target="_blank" rel="noreferrer" href="https://www.notion.so/f3dec368d75b427e9d7d08636876cafa?v=c10e1d052a864d31b81ea1890e783f5a
 ">Cybersecurity Web Resources</a>
             <p className="end-credits__text--2">If you have the time, please fill the feedback form to tell us about your experience with the game</p>
-            <a className="end-credits__text--anchor" href="https://www.questionpro.com/a/TakeSurvey?tt=KWyQx1rbMvg%3D">Feedback form</a>
+            <a className="end-credits__text--anchor" target="_blank" rel="noreferrer" href="https://www.questionpro.com/a/TakeSurvey?tt=KWyQx1rbMvg%3D">Feedback form</a>
             <p className="end-credits__text--footer">Have a nice day!</p>
         </div>
     );
